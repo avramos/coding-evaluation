b0d12f9a87ef579ba2ecb3470161a8911f9fb80d
Arcie Ramos Sr <arcie.ramos@gmail.com>
2023-04-27 16:56:13 -0700

    Created FindPositionAndFill recursive method to traverse the organizational tree and fill in positions as needed based on the Hire inputs.

--- dotnet/MyOrganization/Organization.cs
+++ dotnet/MyOrganization/Organization.cs
@@ -26,8 +26,11 @@ namespace MyOrganization
          */
         public Position? Hire(Name person, string title)
         {
-            //your code here
-            return null;
+            if (person == null || string.IsNullOrEmpty(title)) return null;
+
+            Employee potentialNewEmp = new Employee(person.GetHashCode(), person);
+            
+            return FindPositionAndFill(root, potentialNewEmp, title);
         }
 
         override public string ToString()
@@ -38,11 +41,32 @@ namespace MyOrganization
         private string PrintOrganization(Position pos, string prefix)
         {
             StringBuilder sb = new StringBuilder(prefix + "+-" + pos.ToString() + "\n");
+
             foreach (Position p in pos.GetDirectReports())
             {
                 sb.Append(PrintOrganization(p, prefix + "\t"));
             }
+
             return sb.ToString();
+        }
+
+        private Position? FindPositionAndFill(Position position, Employee potentialEmployee, string title)
+        { 
+            if (position == null || potentialEmployee == null) return null;
+
+            if (position.GetTitle() == title && position.GetEmployee() == null) 
+            { 
+                position.SetEmployee(potentialEmployee); 
+                return position; 
+            }
+
+            // Positional match did not happen; Let's drill down
+            foreach (Position p in position.GetDirectReports())
+            { 
+                FindPositionAndFill(p, potentialEmployee, title);
+            }
+
+            return null;
         }
     }
 }
